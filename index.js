6b43e60ee6cccfdd46d99813c68a02124d09dd76
ArizonaMangoJuice <isaelwebdeveloper@gmail.com>
2021-02-09 22:33:15 -0700

    added repetive js prob wont refactor

---
+++ index.js
@@ -0,0 +1,102 @@
+let firstName = false;
+let lastName =  false;
+let email = false;
+let password = false;
+
+let isEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+['click', 'keyup'].forEach( e => {
+    console.log(e)
+    document.getElementsByName('firstName')[0]
+    .addEventListener(e, event => {
+        let input = document.getElementsByName('firstName')[0];
+        let label = document.getElementById('firstlabel');
+
+        if(event.target.value.length === 0 || event.target.value === ''){
+            input.classList.add('error-border');
+            label.classList.remove('hidden');
+            firstName = false;
+        }
+
+        if(event.target.value.length !== 0){
+            label.classList.add('hidden');
+            input.classList.remove('error-border');
+            firstName = true;
+        }
+    });
+});
+
+
+['click', 'keyup'].forEach( e => {
+    console.log(e)
+    document.getElementsByName('lastName')[0]
+    .addEventListener(e, event => {
+        let input = document.getElementsByName('lastName')[0];
+        let label = document.getElementById('lastlabel');
+
+        if(event.target.value.length === 0 || event.target.value === ''){
+            input.classList.add('error-border');
+            label.classList.remove('hidden');
+            lastName = false;
+        }
+
+        if(event.target.value.length !== 0){
+            label.classList.add('hidden');
+            input.classList.remove('error-border');
+            lastName = true;
+        }
+    });
+});
+
+['click', 'keyup'].forEach( e => {
+    console.log(e)
+    document.getElementsByName('email')[0]
+    .addEventListener(e, event => {
+        let input = document.getElementsByName('email')[0];
+        let label = document.getElementById('email');
+
+        if(!isEmail.test(event.target.value)){
+            input.classList.add('error-border');
+            label.classList.remove('hidden');
+            email = false;
+        }
+
+        if(isEmail.test(event.target.value)){
+            label.classList.add('hidden');
+            input.classList.remove('error-border');
+            email = true;
+        }
+
+        console.log(firstName)
+    });
+});
+
+['click', 'keyup'].forEach( e => {
+    console.log(e)
+    document.getElementsByName('password')[0]
+    .addEventListener(e, event => {
+        let input = document.getElementsByName('password')[0];
+        let label = document.getElementById('password');
+
+        if(event.target.value.length === 0 || event.target.value === ''){
+            input.classList.add('error-border');
+            label.classList.remove('hidden');
+            password = false;
+        }
+
+        if(event.target.value.length !== 0){
+            label.classList.add('hidden');
+            input.classList.remove('error-border');
+            password = true;
+        }
+        console.log(firstName)
+    });
+});
+
+const submit = (e) => {
+    if(firstName && lastName && email && password){
+        return;
+    } else {
+        e.preventDefault;
+    }
+}
